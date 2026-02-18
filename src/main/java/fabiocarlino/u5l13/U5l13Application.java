03316156481337c8bb4ca6130f0b6c2ed41d6613
Leon35-fc <leon.f35@alice.it>
2026-02-18 15:27:57 +0100

    .

---
+++ src/main/java/fabiocarlino/u5l13/U5l13Application.java
@@ -0,0 +1,13 @@
+package fabiocarlino.u5l13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5l13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5l13Application.class, args);
+	}
+
+}
---
+++ src/test/java/fabiocarlino/u5l13/U5l13ApplicationTests.java
@@ -0,0 +1,13 @@
+package fabiocarlino.u5l13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5l13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
